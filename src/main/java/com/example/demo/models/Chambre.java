59c8d05f7c736b58ca2651d24cc075a3bfd2d5c6
soulaima kahla <soulaimakahla@gmail.com>
2021-01-15 00:25:38 +0100

    6

--- src/main/java/com/example/demo/models/Chambre.java
+++ src/main/java/com/example/demo/models/Chambre.java
@@ -1,9 +1,9 @@
 package com.example.demo.models;
 
 public class Chambre {
-<<<<<<< HEAD
+
 int a;
-=======
+
 int b;
->>>>>>> branch 'main' of https://github.com/SoulaimakH/Gestion_hospitaliere.git
+
 }
