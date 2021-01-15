e1ae7f4c54e347de4b22bdd5e4cca3e362b7dbbe
yosr-zouaoui <zwawiyosr@gmail.com>
2021-01-15 11:37:01 +0100

    medecin

--- src/main/java/com/example/demo/models/Medecin.java
+++ src/main/java/com/example/demo/models/Medecin.java
@@ -1,5 +1,28 @@
 package com.example.demo.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name="Medecin")
 public class Medecin {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int code_M;
+	String nom_M;
+	String prenom_M;
+	String tel_M;
+	
+	
+	
+	
 
 }
