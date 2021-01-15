8d92a6705eba137ad8574a1740bf126c8f4e4dcf
yosr-zouaoui <zwawiyosr@gmail.com>
2021-01-15 13:18:17 +0100

    repo

--- src/main/java/com/example/demo/repository/PatientRepository.java
+++ src/main/java/com/example/demo/repository/PatientRepository.java
@@ -1,5 +1,9 @@
 package com.example.demo.repository;
 
-public interface PatientRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.models.Patient;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
 
 }
